1797caa5fb02e8bae959eea4f42a7eee0c82faba
KishieBhenyu <kbhenyu@byupathway.edu>
2025-09-13 21:21:45 +0200

    w2

--- temples.js
+++ temples.js
@@ -1,9 +1,19 @@
-const yearSpan = document.getElementById('currentyear');
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("primaryNav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  menuBtn.textContent = nav.classList.contains("show") ? "X" : "☰";
+});
+
+// Set current year
+const yearSpan = document.getElementById("currentyear");
 if (yearSpan) {
   yearSpan.textContent = new Date().getFullYear();
 }
 
-const lastMod = document.getElementById('lastModified');
+// Set last modified date
+const lastMod = document.getElementById("lastModified");
 if (lastMod) {
   lastMod.textContent = `Last modified: ${document.lastModified}`;
 }
